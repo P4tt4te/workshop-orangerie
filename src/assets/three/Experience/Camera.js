f60bf9b0f1ea6e0ec29dc8bd446b03ba90e2078a
Amandine <quellec.amandine@gmail.com>
2023-01-05 17:03:59 +0100

    feat(camera): fix bug

--- src/assets/three/Experience/Camera.js
+++ src/assets/three/Experience/Camera.js
@@ -10,72 +10,77 @@ export default class Camera {
         this.scene = this.experience.scene
         this.canvas = this.experience.canvas
 
-        this.setInstance()
-        this.moveCamera()
-        //this.setControls();
+    this.setInstance();
+    this.moveCamera();
+    //this.setControls();
+  }
+
+  setInstance() {
+    this.instance = new PerspectiveCamera(
+      35,
+      this.sizes.width / this.sizes.height,
+      0.1,
+      100
+    );
+    this.instance.position.set(0, 1, 5);
+    this.scene.add(this.instance);
+  }
+
+  setControls() {
+    //this.controls = new OrbitControls(this.instance, this.canvas);
+    //this.controls.enableDamping = true;
+  }
+
+  resize() {
+    this.instance.aspect = this.sizes.width / this.sizes.height;
+    this.instance.updateProjectionMatrix();
+  }
+
+  moveCamera() {
+    const self = this;
+    this.experience.goLeft.addEventListener(
+      "click",
+      function () {
+        gsap
+          .timeline()
+          .to(self.instance.position, { x: self.instance.position.x - 3 });
+      },
+      false
+    );
+
+    this.experience.goRight.addEventListener(
+      "click",
+      function () {
+        gsap
+          .timeline()
+          .to(self.instance.position, { x: self.instance.position.x + 3 });
+      },
+      false
+    );
+  }
+
+  navigation(position) {
+    if (position === 3) {
+      this.experience.goRight.style.display = "none";
+    } else if (position === -3) {
+      this.experience.goLeft.style.display = "none";
+    } else {
+      this.experience.goRight.style.display = "block";
+      this.experience.goLeft.style.display = "block";
     }
 
-    setInstance() {
-        this.instance = new PerspectiveCamera(
-            35,
-            this.sizes.width / this.sizes.height,
-            0.1,
-            100
-        )
-        this.instance.position.set(0, 1, 5)
-        this.scene.add(this.instance)
+    if (position === 0 || position === 3 || position === -3) {
+      this.experience.goRight.disabled = false;
+      this.experience.goLeft.disabled = false;
+    } else {
+      this.experience.goRight.disabled = true;
+      this.experience.goLeft.disabled = true;
     }
+  }
 
-    setControls() {
-        //this.controls = new OrbitControls(this.instance, this.canvas);
-        //this.controls.enableDamping = true;
-    }
-
-    resize() {
-        this.instance.aspect = this.sizes.width / this.sizes.height
-        this.instance.updateProjectionMatrix()
-    }
-
-    moveCamera() {
-        const self = this
-        this.experience.goLeft.addEventListener(
-            'click',
-            function () {
-                gsap.timeline()
-                    .to(self.instance.position, { x: self.instance.position.x })
-                    .to(self.instance.position, {
-                        x: self.instance.position.x - 3,
-                    })
-            },
-            false
-        )
-
-        this.experience.goRight.addEventListener(
-            'click',
-            function () {
-                gsap.timeline()
-                    .to(self.instance.position, { x: self.instance.position.x })
-                    .to(self.instance.position, {
-                        x: self.instance.position.x + 3,
-                    })
-            },
-            false
-        )
-    }
-
-    navigation(position) {
-        if (position === 3) {
-            this.experience.goRight.style.display = 'none'
-        } else if (position === -3) {
-            this.experience.goLeft.style.display = 'none'
-        } else {
-            this.experience.goRight.style.display = 'block'
-            this.experience.goLeft.style.display = 'block'
-        }
-    }
-
-    update() {
-        this.navigation(this.instance.position.x)
-        //this.controls.update();
-    }
+  update() {
+    console.log(this.instance.position.x);
+    this.navigation(this.instance.position.x);
+    //this.controls.update();
+  }
 }
