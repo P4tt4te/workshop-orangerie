33ae09c284a28325b3627c9a2e5762122a543947
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-01-05 14:31:31 +0100

    feat: add fog & change floor

--- src/assets/three/Experience/World/World.js
+++ src/assets/three/Experience/World/World.js
@@ -1,24 +1,24 @@
-import Experience from "../Experience";
-import Environment from "./Environment";
-import Floor from "./../World/Floor";
-import Fox from "./../World/Fox";
+import Experience from '../Experience'
+import Environment from './Environment'
+import Floor from './../World/Floor'
+import Fox from './../World/Fox'
 
 export default class World {
-  constructor() {
-    this.experience = new Experience();
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
 
-    // Wait for resources
-    this.resources.on("ready", () => {
-      // Setup
-      this.floor = new Floor();
-      this.fox = new Fox();
-      this.environment = new Environment();
-    });
-  }
+        // Wait for resources
+        this.resources.on('ready', () => {
+            // Setup
+            this.floor = new Floor()
+            this.fox = new Fox()
+            this.environment = new Environment()
+        })
+    }
 
-  update() {
-    if (this.fox) this.fox.update();
-  }
+    update() {
+        if (this.fox) this.fox.update()
+    }
 }
