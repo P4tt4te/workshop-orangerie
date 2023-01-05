fdbd6367a9b64ef89b3c58336d7bcfc10c7ada52
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-01-05 22:27:03 +0100

    fix: bad rebasing for Experience.js

--- src/assets/three/Experience/Experience.js
+++ src/assets/three/Experience/Experience.js
@@ -12,71 +12,10 @@ import { Mesh, Scene } from 'three'
 let instance = null
 
 export default class Experience {
-  constructor(_canvas, goLeft, goRight) {
-    // Singleton
-    if (instance) {
-      return instance;
-    }
-    instance = this;
-
-    // Global access
-    window.experience = this;
-
-    // Options
-    this.canvas = _canvas;
-    this.goLeft = goLeft;
-    this.goRight = goRight;
-
-    // Setup
-    this.debug = new Debug();
-    this.sizes = new Sizes();
-    this.time = new Time();
-    this.scene = new Scene();
-    this.resources = new Resources(sources);
-    this.camera = new Camera();
-    this.renderer = new Renderer();
-    this.world = new World();
-
-    // Resize event
-    this.sizes.on("resize", () => {
-      this.resize();
-    });
-
-    // Time tick event
-    this.time.on("tick", () => {
-      this.update();
-    });
-  }
-
-  resize() {
-    this.camera.resize();
-    this.renderer.resize();
-  }
-
-  update() {
-    this.camera.update();
-    this.world.update();
-    this.renderer.update();
-  }
-
-  destroy() {
-    this.sizes.off("resize");
-    this.time.off("tick");
-
-    // Traverse the whole scene
-    this.scene.traverse((child) => {
-      // Test if it's a mesh
-      if (child instanceof Mesh) {
-        child.geometry.dispose();
-
-        // Loop through the material properties
-        for (const key in child.material) {
-          const value = child.material[key];
-
-          // Test if there is a dispose function
-          if (value && typeof value.dispose === "function") {
-            value.dispose();
-          }
+    constructor(_canvas, goLeft, goRight) {
+        // Singleton
+        if (instance) {
+            return instance
         }
         instance = this
 
@@ -85,6 +24,8 @@ export default class Experience {
 
         // Options
         this.canvas = _canvas
+        this.goLeft = goLeft
+        this.goRight = goRight
 
         // Setup
         this.debug = new Debug()
