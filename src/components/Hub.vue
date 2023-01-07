fa05a362c8f18a171d6f9fb5b595200c22c58f02
victorcngo <hello@victorcango.fr>
2023-01-07 17:32:49 +0100

    feat(components): create Hub

---
+++ src/components/Hub.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="Hub">
+        <div>Portrait 1</div>
+        <div>Portrait 2</div>
+        <div>Portrait 3</div>
+    </section>
+</template>
+
+<script>
+export default {
+    components: {},
+}
+</script>
+
+<style lang="scss">
+
+</style>
