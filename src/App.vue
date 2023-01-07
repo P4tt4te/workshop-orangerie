510f5d781debeca2144b708aa5b16d616bb50c9c
victorcngo <hello@victorcango.fr>
2023-01-07 17:04:03 +0100

    chore: clean useless code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
     <div id="app">
         <header>
             <div class="wrapper">
-                <HelloWorld msg="You did it!" />
-
                 <nav>
                     <router-link to="/">Home</router-link>
                     <router-link to="/about">About</router-link>
                 </nav>
             </div>
         </header>
-
         <router-view />
     </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script>
 import TheWelcome from '@/components/TheWelcome.vue'
-import Experience from '@/components/Experience.vue'
 
 export default {
     components: {
         TheWelcome,
-        Experience,
     },
 }
 </script>
@@ -13,6 +11,5 @@ export default {
 <template>
     <main>
         <TheWelcome />
-        <Experience />
     </main>
 </template>
