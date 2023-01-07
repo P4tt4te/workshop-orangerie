cdeb419c71e45987d0956e5a899b33634f57e20d
victorcngo <hello@victorcango.fr>
2023-01-07 18:57:24 +0100

    test(components): create SinglePortrait

--- src/components/Hub.vue
+++ src/components/Hub.vue
@@ -1,17 +1,80 @@
 <template>
     <section class="Hub">
-        <div>Portrait 1</div>
-        <div>Portrait 2</div>
-        <div>Portrait 3</div>
+        <div
+            class="Portrait"
+            v-for="(portrait, index) in portraits"
+            :key="index"
+            @click="showModal(index)"
+        >
+            <h2 class="Portrait__title">{{ portrait.title }}</h2>
+            <p>{{ portrait.date }}</p>
+        </div>
+        <SinglePortrait
+            v-if="isModalActive"
+            :portraits="portraits"
+            :currentIndex="currentIndex"
+            @closeModal="closeModal"
+        />
     </section>
 </template>
 
 <script>
+import SinglePortrait from '@/components/SinglePortrait.vue'
+
 export default {
-    components: {},
+    components: {
+        SinglePortrait,
+    },
+    data() {
+        return {
+            currentIndex: 0,
+            isModalActive: false,
+            portraits: [
+                {
+                    id: '0',
+                    title: 'Le Petit Pâtissier',
+                    date: '1922-1923',
+                    image: 'img1',
+                },
+                {
+                    id: '1',
+                    title: 'La Fiancée',
+                    date: '1923',
+                    image: 'img2',
+                },
+                {
+                    id: '2',
+                    title: 'Enfant de chœur',
+                    date: '1927-1928',
+                    image: 'img3',
+                },
+            ],
+        }
+    },
+    methods: {
+        showModal(index) {
+            this.isModalActive = true
+            this.currentIndex = index
+        },
+        closeModal() {
+            this.isModalActive = false
+        },
+    },
 }
 </script>
 
 <style lang="scss">
+.Hub {
+    display: flex;
 
+    .Portrait {
+        border: 0.1rem solid $grey;
+        height: 30rem;
+        width: 20rem;
+
+        &:not(:last-child) {
+            margin-right: 2rem;
+        }
+    }
+}
 </style>
---
+++ src/components/SinglePortrait.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="SinglePortrait">
+        <div class="SinglePortrait__meta">
+            <h2>{{ currentPortrait.title }}</h2>
+            <p>{{ currentPortrait.date }}</p>
+            <p>{{ currentPortrait.id }}</p>
+        </div>
+        <button class="SinglePortrait__prev" @click="goPrev">Prev</button>
+        <br />
+        <button class="SinglePortrait__next" @click="goNext">Next</button>
+        <div class="SinglePortrait__close" @click="$emit('closeModal')">
+            Close
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        portraits: Array,
+        currentIndex: Number,
+    },
+    data() {
+        return {
+            index: this.$props.currentIndex,
+        }
+    },
+    methods: {
+        goPrev() {
+            console.log(this.index)
+            this.index =
+                this.index - 1 < 0 ? this.portraits.length - 1 : this.index - 1
+        },
+        goNext() {
+            console.log(this.index)
+            this.index = (this.index + 1) % this.portraits.length
+        },
+    },
+    computed: {
+        currentPortrait() {
+            return this.portraits[this.index]
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.SinglePortrait {
+    position: absolute;
+    background-color: $white;
+    width: 100%;
+    height: 100%;
+}
+</style>
