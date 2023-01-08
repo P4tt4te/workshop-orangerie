eff12fd8f48e6aa9191943cb082cd48e7411ea08
victorcngo <hello@victorcango.fr>
2023-01-08 01:55:28 +0100

    chore(vite.config): prettier and update additionalData

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,30 @@
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite"
-import legacy from "@vitejs/plugin-legacy"
-import vue2 from "@vitejs/plugin-vue2"
+import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue2(),
         legacy({
-            targets: ["ie >= 11"],
-            additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+            targets: ['ie >= 11'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
         }),
     ],
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: '@use "@/assets/scss/index.scss" as *;',
+                additionalData: `
+                  @import "./src/assets/scss/_colors.scss";
+                `,
             },
         },
     },
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 })
