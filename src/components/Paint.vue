ea966531468753410c1723ff9677f1cf0d518184
Edward B <57622377+P4tt4te@users.noreply.github.com>
2023-01-08 19:16:11 +0100

    style: add sticker animation

--- src/components/Paint.vue
+++ src/components/Paint.vue
@@ -1,7 +1,10 @@
 <template>
     <div :class="'paintContainer'">
         <img :class="'paint'" :src="image" alt="tableau" />
-        <img :class="'sticker'" :src="sticker" alt="sticker" />
+        <div :class="'sticker'">
+            <img :class="'stickerFront'" :src="sticker" alt="sticker" />
+            <div :class="'stickerBack'"></div>
+        </div>
     </div>
 </template>
 
@@ -35,16 +38,77 @@ export default {
     height: 850px;
     width: fit-content;
     position: relative;
+    &:hover .sticker {
+        transform: translate3d(0, 0, 0) scale(1) rotate(-45deg);
+        transition-delay: 0s;
+        opacity: 1;
+        .stickerFront {
+            height: 100px;
+        }
+
+        .stickerBack {
+            top: 0;
+
+            &:before {
+                top: -100px;
+            }
+        }
+    }
 }
 .paint {
     height: 100%;
     width: max-content;
 }
 .sticker {
+    display: block;
     position: absolute;
     top: 0;
+    right: 0;
+    bottom: 0;
     left: 0;
+    margin: auto;
+    z-index: 5;
     width: 100px;
     height: 100px;
+    border-radius: 50%;
+    transition: 0.2s 0.2s;
+    transform: translate3d(10px, 20px, 0) scale(1.5) rotate(-45deg);
+    opacity: 0;
+}
+
+.stickerBack,
+.stickerFront {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    transition: 0.35s;
+
+    &:before {
+        content: ' ';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        transition: 0.35s;
+    }
+}
+
+.stickerFront {
+    height: 0;
+}
+.stickerBack {
+    top: 100px;
+    overflow: hidden;
+
+    &:before {
+        background-color: #f7e5c0;
+        box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.4);
+        top: 0;
+    }
 }
 </style>
