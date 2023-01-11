d117a48729bcef660148c1f61fd2bb36d14019bb
victorcngo <hello@victorcango.fr>
2023-01-11 20:13:31 +0100

    chore(Subtitles): tweak position

--- src/components/Subtitles.vue
+++ src/components/Subtitles.vue
@@ -10,12 +10,13 @@ export default {
     },
 }
 </script>
+
 <style lang="scss" scoped>
 .subtitles {
     text-align: center;
     color: white;
     position: absolute;
-    bottom: 0;
+    bottom: 6rem;
     width: 100%;
     z-index: 10;
     font-size: 21px;
